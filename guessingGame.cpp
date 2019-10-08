a4aee9e87138f49fd619c891bc45a3550fa6dbea
Gabriel Mendez-Frances <mendezgabriel2000@gmail.com>
2019-10-08 15:49:44 -0500

    Created a small guessing game
    Used rand() by implementing the <stdlib.h> library. Also used the <time.h> in conjunction with srand() to get a different seed value for rand().

---
+++ guessingGame.cpp
@@ -0,0 +1,32 @@
+// Guessing game
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+	int n;
+	time_t t;
+	char ch = '\n';
+	int guess;
+
+	srand((unsigned)(time(&t)));
+	n = (rand() % 6) + 1;
+	printf("Welcome to the Guessing game.\n");
+	while (ch != 'q') {
+		printf("Choose a number: ");
+		scanf("%d", &guess);
+		if (guess > n) {
+			printf("You guess is too high!\n");
+		}
+		else if (guess < n) {
+			printf("Your guess is too low!\n");
+		}
+		else {
+			printf("You got it!\n");
+			break;
+		}
+		scanf("%c", &ch);
+	}
+}
