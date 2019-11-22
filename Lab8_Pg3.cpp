c542ad37ac98b3109c574e2c72626bbc18fa895a
Gabriel Mendez-Frances <gjf0004@uah.edu>
2019-11-21 23:46:34 -0600

    Update Lab8_Pg3.cpp

--- Lab8_Pg3.cpp
+++ Lab8_Pg3.cpp
@@ -1,15 +1,21 @@
+// Lab 8 Program 3
+// CS 102-02
+// Gabriel Mendez-Frances
+// 11-21-2019
+// Write a C program to read array elements and print with addresses
+
 #include <stdio.h> 
 
-int main() { 
+int main() {
 	int arr[100];       //declare integer array 
 	int *pa;            //declare an integer pointer 
-	int i, number;
+	int *number;
+	int i;
 	pa = arr; //assign base address of array
 
 	for (i = 0; i < 100; i++) { //reading through pointer 
-		number = pa[i];
-		printf("Address of index %d = %d\n", i, number);
+		number = &pa[i];
+		printf("Address of index %d = %d\n", i, number); //printing the address
 	}
-	//printing the address
-return 0;
+	return 0;
 }
